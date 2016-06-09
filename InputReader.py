5e1ed62d6d54e2da775bbdd4f32c06c807500324
Matthew <mmschiav@buffalo.edu>
2016-06-09 16:59:22 -0500

    Started working on the input reader file so it can read the new inputs. Need to figure out how it is going to take in symbols and convert them using the sympy Symbol function

--- InputReader.py
+++ InputReader.py
@@ -1,49 +1,87 @@
-def Input_filereader(inFile):
-    inputs = []
-    start = 0
-    with open(inFile) as input:
-        for i, line in enumerate(input):
-            if i == 11:
+from sympy import Symbol
+
+class Reader:
+
+
+
+
+    def __init__(self,File):
+        self.inFile = File
+
+
+
+
+    def Input_filereader(self):
+        inputs = []
+        start = 0
+        with open(self.inFile) as input:
+            for i, line in enumerate(input):
+                if i == 9:
                 
-                for j in range(len(line)):
-                    if line[j] == ":":
-                        start = j + 1
-                        break
-                TripTime2 = ""
-                for k in range(start,len(line)):
-                    TripTime2 = TripTime2 + line[k]
-            if i == 13:
-                for j in range(len(line)):
-                    if line[j] ==":":
-                        start = j + 1
-                        break
-                KillSpeed = ""
-                for k in range(start,len(line)):
-                    KillSpeed = KillSpeed + line[k]
+                    for j in range(len(line)):
+                        if line[j] == ":":
+                            start = j + 1
+                            break
+                    Symbols = ""
+                    for k in range(start,len(line)):
+                        Symbols = Symbols + line[k]
 
-            if i == 15:
-                for j in range(len(line)):
-                    if line[j] == ":":
-                        start = j+1
-                        break
-                Events = ""
-                for k in range(start,len(line)):
-                    Events = Events + line[k]
-            if i == 17:
-                for j in range(len(line)):
-                    if line[j] == ":":
-                        start = j + 1
-                        break
-                Bin = ""
-                for k in range(start,len(line)):
-                    Bin = Bin + line[k]
-    
-        inputs.append(int(TripTime2.replace(" ",""))*60)
-        inputs.append(int(KillSpeed.replace(" ","")))
-        inputs.append(int(Events.replace(" ","")))
-        inputs.append(float(Bin.replace(" ","")))
-# Inputs array [ Time of trip in seconds, average kill time in seconds, Number of Events ]
-    
-    input.close()
+                if i == 11:
+                
+                    for j in range(len(line)):
+                        if line[j] == ":":
+                            start = j + 1
+                            break
+                    Internal = ""
+                    for k in range(start,len(line)):
+                        Internal = Internal + line[k]
+                if i == 13:
+                    for j in range(len(line)):
+                        if line[j] ==":":
+                            start = j + 1
+                            break
+                    External = ""
+                    for k in range(start,len(line)):
+                        External = External + line[k]
 
-    return inputs
+                if i == 15:
+                    for j in range(len(line)):
+                        if line[j] == ":":
+                            start = j+1
+                            break
+                    Propagators = ""
+                    for k in range(start,len(line)):
+                        Propagators = Propagators + line[k]
+            Symbols = Symbols.replace(" ","")
+            Internal = Internal.replace(" ","")
+            External = External.replace(" ","")
+            Propagators = Propagators.replace(" ","")
+            Symbols = Symbols.replace("\n","")
+            
+            inputs.append(Internal.replace("\n",""))
+            inputs.append(External.replace("\n",""))
+            inputs.append(Propagators.replace("\n",""))
+            
+
+        input.close()
+        for i in range(len(inputs)):
+            
+            inputs[i] = filter(lambda a: a != '[', list(inputs[i]))
+            inputs[i] = filter(lambda a: a != ']', list(inputs[i]))
+            inputs[i] = filter(lambda a: a != ',', list(inputs[i]))
+        
+
+
+        for i in range(len(Symbols)):
+            a = Symbols[i] # error here 'str' object does not support item assignment
+            Symbols[i] = Symbol('a')
+        for i in range(len(inputs)):
+            for j in range(len(inputs[i])):
+                inputs[i][j] = int(inputs[i][j])
+
+
+        return inputs
+
+Reader=Reader("input.DAT")
+
+print(Reader.Input_filereader())
