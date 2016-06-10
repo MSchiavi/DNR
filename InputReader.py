1b3bc369514585aa8701197a18104918ddbe002c
Matthew <mmschiav@buffalo.edu>
2016-06-10 11:46:33 -0500

    worked on the file reader everything is working but putting the propagators in a mathematical format. Manipulating the strings in python and keeping track of what operators is where is becoming difficult

--- InputReader.py
+++ InputReader.py
@@ -22,9 +22,9 @@ class Reader:
                         if line[j] == ":":
                             start = j + 1
                             break
-                    Symbols = ""
+                    input_Symbols = ""
                     for k in range(start,len(line)):
-                        Symbols = Symbols + line[k]
+                        input_Symbols = input_Symbols + line[k]
 
                 if i == 11:
                 
@@ -49,39 +49,85 @@ class Reader:
                         if line[j] == ":":
                             start = j+1
                             break
-                    Propagators = ""
+                    Props = ""
                     for k in range(start,len(line)):
-                        Propagators = Propagators + line[k]
-            Symbols = Symbols.replace(" ","")
-            Internal = Internal.replace(" ","")
-            External = External.replace(" ","")
-            Propagators = Propagators.replace(" ","")
-            Symbols = Symbols.replace("\n","")
-            
-            inputs.append(Internal.replace("\n",""))
-            inputs.append(External.replace("\n",""))
-            inputs.append(Propagators.replace("\n",""))
-            
+                        Props = Props + line[k]
 
+            
         input.close()
-        for i in range(len(inputs)):
-            
-            inputs[i] = filter(lambda a: a != '[', list(inputs[i]))
-            inputs[i] = filter(lambda a: a != ']', list(inputs[i]))
-            inputs[i] = filter(lambda a: a != ',', list(inputs[i]))
+        input_Symbols = cleanup(input_Symbols)
+        Internal = cleanup(Internal)
+        External = cleanup(External)
+        Props = Props.replace("[","")
+        Props = Props.replace("]","")
         
-
-
-        for i in range(len(Symbols)):
-            a = Symbols[i] # error here 'str' object does not support item assignment
-            Symbols[i] = Symbol('a')
-        for i in range(len(inputs)):
-            for j in range(len(inputs[i])):
-                inputs[i][j] = int(inputs[i][j])
-
+        Symbols = math_list(input_Symbols)
+        Internal = math_list(Internal)
+        External = math_list(External)
+        print(Props)
+        print(props_list(Props))
 
         return inputs
+#replaces some unwanted nonsense from the input file
+def cleanup(string):
+    string = string.replace(" ","")
+    string = string.replace("[","")
+    string = string.replace("\n","")
+    string = string.replace("]","")
+    string = string.replace(",","")
+    return string
+# Just makes the internal and external inputs math symbols
+def math_list(String):
+    result = []
+    for i in range(len(String)):
+        result.append(Symbol(String[i]))
+    return result    
+#Lists out the propagators and will eventually put then in  mathematical format. 
 
+def props_list(String):
+    j = 0
+    Propagators = []
+    for i in range(len(String)):
+        if j == 2:
+            temp = ""
+            for k in range(start,i+1):
+                temp = temp + String[k]
+            Propagators.append(temp)
+            j = 0 
+        
+        if String[i] == '(':
+            start = i 
+        
+        if String[i] == '*':
+            j+=1
+
+    #Trying to get the propagators out of a string and in a mathematical format
+    #so they can be manipulated. 
+    for i in range(len(Propagators)):
+        times = 0
+        number_times = 0
+        Open_P = Propagators[i].count('(')
+        Close_P = Propagators[i].count(')')
+        Propagators[i] = Propagators[i].replace("(","")
+        Propagators[i] = Propagators[i].replace(")","")
+        print(Propagators[i])
+        for j in range(len(Propagators[i])):
+            if Propagators[i][j] == '-':
+                minus = j
+            if Propagators[i][j] == '*':
+                number_times += 1 
+                times = j
+        power = int(Propagators[i][times + 1])
+        Propagators[i] = Propagators[i].replace(Propagators[i][times + 1],"")
+        Propagators[i] = Propagators[i].replace("-","")
+        Propagators[i] = Propagators[i].replace("*","")
+
+        print(minus)
+        print(times)
+
+
+    return Propagators    
+            
 Reader=Reader("input.DAT")
 
-print(Reader.Input_filereader())
+Reader.Input_filereader()
