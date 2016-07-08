8e0e1c274a74a0d83516de1e87eb376f321a250e
Matthew <mmschiav@buffalo.edu>
2016-07-08 15:11:58 -0500

    Almost completed the IBP.py method which will output a math version of the IBP. String IBP no longer works but can be fixed easily

--- IBP.py
+++ IBP.py
@@ -12,6 +12,9 @@ from math import *
 from sympy import *
 from initialization import *
 from representation import *
+import time
+
+start_time = time.time()
 One = type(Abs(1)/Abs(1))
 
 class IBP(object):
@@ -24,8 +27,9 @@ class IBP(object):
 		Brew = Initialization()
 		print(Brew.External_Matrix())
 		Props = Brew.get_props()
-		output = self.rep(Props,Brew.find_squares(),Brew.External_Matrix())
-		self.IBP_String = self.output_reader(output)
+		self.output = self.rep(Props,Brew.find_squares(),Brew.External_Matrix())
+		#self.IBP_String = self.output_reader(self.output)
+		self.math_output = self.math_output(self.output)
 
 
 	def rep(self,Props,Squares,External_Matrix):
@@ -64,18 +68,20 @@ class IBP(object):
 		for i in range(len(coeffs)):
 			temp[i] = transpose(Matrix(coeffs[i]))*External_Matrix
 		
-		output = [[] for y in range(len(temp))]
-
+		#output = [[] for y in range(len(temp))]
+		output = []
 		#Creating the IBP output. Ladders is an object which represents creation and annihilation operators. The first arguement is which propagator 
 		# it will effect and the second is if it is creation ('+') or annihilation ('-'). indicies is just the "a" term from the propagators. 
-		print temp
+		#print temp
 		
 		for i in range(len(temp)):
 			for j in range(len(temp[i])):
 				if j < (len(temp[i])-1):
-					output[i].append([temp[i][j],ladders(j + 1,'-'),ladders(i + 1,'+'),indicies(i + 1)])
+					#output[i].append([temp[i][j],ladders(j + 1,'-'),ladders(i + 1,'+'),indicies(i + 1)])
+					output.append([temp[i][j],ladders(j + 1,'-'),ladders(i + 1,'+'),Symbol("a"+str(i+1))])
 				else:
-					output[i].append([temp[i][j],1,ladders(i + 1,'+'),indicies(i + 1)])
+					#output[i].append([temp[i][j],1,ladders(i + 1,'+'),indicies(i + 1)])
+					output.append([temp[i][j],1,ladders(i + 1,'+'),Symbol("a"+str(i+1))])
 
 		output.append([diff(self.x,self.y)*Symbol('d')])
 
@@ -99,23 +105,128 @@ class IBP(object):
 					elif type(output[i][j][0]) is Integer and output[i][j][0] > 1:
 
 						readable = readable + "" + " - " + str(output[i][j][0])  + "a("+ str(output[i][j][3].get_index())+")" + "Y("+str(output[i][j][1].get_index())+ "," + output[i][j][1].get_op()+ ")" + "Y("+ str(output[i][j][2].get_index())+ "," + output[i][j][2].get_op()+ ")" 
-						#this will have to be coded at some point
+						
 					else:
 						if output[i][j][0].args[0] == 1:
+
 							readable = readable + "" + " - a("+ str(output[i][j][3].get_index())+ ")" + str(output[i][j][0]) + "Y("+ str(output[i][j][2].get_index())+ "," + output[i][j][2].get_op()+ ")" 
+
 						elif output[i][j][0].args[0] == -1:
+
 							readable = readable + "" + " + a("+ str(output[i][j][3].get_index())+ ")" + str(-1*output[i][j][0]) + "Y("+ str(output[i][j][2].get_index())+ "," + output[i][j][2].get_op()+ ")" 
 
 
 		readable = readable + " + " + str(output[3][0])
 		
 		return readable
+	
+
+
+
+	def math_output(self,ini_output):
+		fin_output = 0
+		op_count = 0
+		zero_count = 0
+		sym_bro = []
+		IBP_OPS = []
+
+	#####Something is wrong here not grabbing the correct ladder objects ##########
+		for i in range(len(ini_output) - 1):
+			if ini_output[i][0] != 0 and len(ini_output[i]) != 1:
+				if ini_output[i][1] != 1:
+					
+					sym_bro.append(ini_output[i][1])
+					op_count += 1
+				if ini_output[i][2] != 1:
+					
+					sym_bro.append(ini_output[i][1])
+					op_count += 1
+			elif ini_output[i][0] == 0:
+				zero_count += 1
+
+    ########################################
+		print(sym_bro)
+		print (op_count,"       op count")
+
+		sym = symbols("I:"+str(op_count))
+		x = 0
+		for i in range(len(ini_output) - 1):
+			if ini_output[i][0] != 0 and len(ini_output[i]) != 1:
+				if ini_output[i][1] != 1:
+					ini_output[i][1] = sym[x]
+					x += 1
+				if ini_output[i][2] != 1:
+					ini_output[i][2] = sym[x]
+					x += 1 
+
+		#print(sym_bro)
+		#for i in range(len(sym_bro)):
+		#	if sym_bro[i] != 1:
+		#		print(sym_bro[i].get_op())
+		
+
+		for i in range(len(ini_output) - 1):
+			if ini_output[i][0] != 0:
+				if ini_output[i][1] != 1 or ini_output[i][2] != 1:
+					for j in range(len(sym)):
+						if sym[j] == ini_output[i][1]:
+							op_1 = j
+						if sym[j] == ini_output[i][2]:
+							op_2 = j
+					if sym_bro[op_1] == 1 or sym_bro[op_2] == 1:
+						continue
+					else:
+						if sym_bro[op_1].get_index() == sym_bro[op_2].get_index():
+
+
+							if sym_bro[op_1].get_op() != sym_bro[op_2].get_op():
+							
+								ini_output[i][1] = 1
+								ini_output[i][2] = 1
+								
+								del op_1
+								del op_2 
+						else:
+							del op_1
+							del op_2 
+						
+
+		for i in range(len(ini_output)-1):
+			if ini_output[i][0] != 0:
+				fin_output = fin_output + ini_output[i][0]*ini_output[i][1]*ini_output[i][2]*ini_output[i][3]
+		fin_output = fin_output + ini_output[len(ini_output) - 1][0]
+
+
+
+		print("========================================================================================================")
+		IBP_OPS.append(fin_output)
+		IBP_OPS.append(sym_bro)
+		return IBP_OPS
+#
+#		for i in range(len(output) - 1 ):
+#			for j in range(len(output[i])):
+#				if output[i][j][0] != 0:
+#					if output[i][j][0] == 1:
+#
+#
+#					elif type(output[i][j][0]) is Integer and output[i][j][0] > 1:
+#
+#					else:
+#						if output[i][j][0].args[0] == 1:
+#
+#						elif output[i][j][0].args[0] == -1:
+
 
 	def read_IBP(self):
 		return self.IBP_String
-
+	def get_output(self):
+		return self.output
+	def get_math_output(self):
+		return self.math_output
 
 x = Symbol('k')
 y = Symbol('k')
 IBP = IBP(x,y)
-print(IBP.read_IBP())
+#print(IBP.get_output())
+print(IBP.get_math_output())
+print("Run time:",'%.3f'%(time.time()-start_time) )
