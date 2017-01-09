edd3c83d77e0e41395171cc902f0c730f3e39f34
Matthew Schiavi <matthewschiavi@Matthews-MacBook-Pro.local>
2017-01-09 14:08:10 -0500

    pls

--- All_IBP.py
+++ All_IBP.py
@@ -27,72 +27,18 @@ for i in range(len(External)):
 		print(External[i],Internal[j])
 		print(_IBP.Get_IBP(External[i],Internal[j]))
 		
-		
-		#print(_IBP.get_math_output()[1][0].get_index())
-		#print(_IBP.get_math_output()[1][0].get_op())
+		#print "yo"
 
-		#print(_IBP.get_math_output()[1][1].get_index())
-		#print(_IBP.get_math_output()[1][1].get_op())
+		#print(_IBP.Get_IBP(External[i],Internal[j])[1][2].get_index())
+		#print(_IBP.Get_IBP(External[i],Internal[j])[1][2].get_op())
 
-		#print(_IBP.get_math_output()[1][4].get_index())
-		#print(_IBP.get_math_output()[1][4].get_op())
+		#print(_IBP.Get_IBP(External[i],Internal[j])[1][3].get_index())
+		#print(_IBP.Get_IBP(External[i],Internal[j])[1][3].get_op())
+				
+		#print(_IBP.Get_IBP(External[i],Internal[j])[1][4].get_index())
+		#print(_IBP.Get_IBP(External[i],Internal[j])[1][4].get_op())
 
-		#print(_IBP.get_math_output()[1][5].get_index())
-		#print(_IBP.get_math_output()[1][5].get_op())
 
-		#print(_IBP.get_math_output()[1][6].get_index())
-		#print(_IBP.get_math_output()[1][6].get_op())
-
-		#print(_IBP.get_math_output()[1][7].get_index())
-		#print(_IBP.get_math_output()[1][7].get_op())
-
-		#print(_IBP.get_math_output()[1][8].get_index())
-		#print(_IBP.get_math_output()[1][8].get_op())
-
-		#print(_IBP.get_math_output()[1][9].get_index())
-		#print(_IBP.get_math_output()[1][9].get_op())
-
-		#print(_IBP.get_math_output()[1][10].get_index())
-		#print(_IBP.get_math_output()[1][10].get_op())
-
-		#print(_IBP.get_math_output()[1][11].get_index())
-		#print(_IBP.get_math_output()[1][11].get_op())
-
-		#print(_IBP.get_math_output()[1][12].get_index())
-		#print(_IBP.get_math_output()[1][12].get_op())
-
-		#print(_IBP.get_math_output()[1][13].get_index())
-		#print(_IBP.get_math_output()[1][13].get_op())
-
-		#print(_IBP.get_math_output()[1][14].get_index())
-		#print(_IBP.get_math_output()[1][14].get_op())
-
-		#print(_IBP.get_math_output()[1][15].get_index())
-		#print(_IBP.get_math_output()[1][15].get_op())
-
-		#print(_IBP.get_math_output()[1][18].get_index())
-		#print(_IBP.get_math_output()[1][18].get_op())
-
-		#print(_IBP.get_math_output()[1][19].get_index())
-		#print(_IBP.get_math_output()[1][19].get_op())
-
-		#print(_IBP.get_math_output()[1][20].get_index())
-		#print(_IBP.get_math_output()[1][20].get_op())
-
-		#print(_IBP.get_math_output()[1][21].get_index())
-		#print(_IBP.get_math_output()[1][21].get_op())
-
-		#print(_IBP.get_math_output()[1][22].get_index())
-		#print(_IBP.get_math_output()[1][22].get_op())
-
-		#print(_IBP.get_math_output()[1][23].get_index())
-		#print(_IBP.get_math_output()[1][23].get_op())
-
-		#print(_IBP.get_math_output()[1][24].get_index())
-		#print(_IBP.get_math_output()[1][24].get_op())
-
-		#print(_IBP.get_math_output()[1][25].get_index())
-		#print(_IBP.get_math_output()[1][25].get_op())
 
 
 for i in range(len(Internal)):
@@ -101,37 +47,25 @@ for i in range(len(Internal)):
 		print(_IBP.Get_IBP(Internal[i],Internal[j]))
 				
 
-		#print(_IBP.get_math_output()[1][2].get_index())
-		#print(_IBP.get_math_output()[1][2].get_op())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][2].get_index())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][2].get_op())
 
-		#print(_IBP.get_math_output()[1][3].get_index())
-		#print(_IBP.get_math_output()[1][3].get_op())
-
-		#print(_IBP.get_math_output()[1][4].get_index())
-		#print(_IBP.get_math_output()[1][4].get_op())
-
-		#print(_IBP.get_math_output()[1][7].get_index())
-		#print(_IBP.get_math_output()[1][7].get_op())
-
-		#print(_IBP.get_math_output()[1][8].get_index())
-		#print(_IBP.get_math_output()[1][8].get_op())
-
-		#print(_IBP.get_math_output()[1][9].get_index())
-		#print(_IBP.get_math_output()[1][9].get_op())
-
-		#print(_IBP.get_math_output()[1][12].get_index())
-		#print(_IBP.get_math_output()[1][12].get_op())
-
-		#print(_IBP.get_math_output()[1][13].get_index())
-		#print(_IBP.get_math_output()[1][13].get_op())
-
-		#print(_IBP.get_math_output()[1][16].get_index())
-		#print(_IBP.get_math_output()[1][16].get_op())
-
-		#print(_IBP.get_math_output()[1][17].get_index())
-		#print(_IBP.get_math_output()[1][17].get_op())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][3].get_index())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][3].get_op())
+				
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][4].get_index())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][4].get_op())
 		
-		
+
+
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][7].get_index())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][7].get_op())
+
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][8].get_index())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][8].get_op())
+				
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][9].get_index())
+		#print(_IBP.Get_IBP(Internal[i],Internal[j])[1][9].get_op())
 
 
 #_IBP = IBP(Internal[1],Internal[0])
